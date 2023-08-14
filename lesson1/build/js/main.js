52c3abaf66b07c1fc2d0bbcb08e475f76351d404
Nahid Faraji <nahidihealthscreen@gmail.com>
2023-08-14 14:27:52 +0600

    lesson 01

---
+++ lesson1/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
+let username = "Nahid";
+console.log(username);
---
+++ lesson1/src/main.ts
@@ -0,0 +1,14 @@
+
+let a: number = 12;
+let b: number = 6;
+let c: number = 2;
+
+console.log(a / b);
+
+console.log(c * b);
+
+let username: string = "Nahid"
+
+console.log(username);
+
+
